7980161990b3baf2f36a727665c5999f7bb3c197
BrunoRafael14 <brunorafaelfilho10@gmail.com>
2026-03-16 19:54:39 -0300

    adicionando demais métodos da rota usuário

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,21 +1,34 @@
 import express from 'express'
+import db from '../db/connection.js'
 
 const router = express.Router();
 const usuarios = [];
 
-router.get('/', (request,response) => {
-    response.status(200).json(usuarios)
+router.get('/', async (request,response) => {
+    const resultado = await db.query('SELECT * FROM usuarios');
+    response.status(200).json(resultado.rows)
 });
 
-router.get('/:id', (request,response) => {
-    const id = parseInt(request.params.id);
-    const usuario = usuarios.find(item => item.id === id);
-    response.status(200).json(usuarios[id])
+router.get('/:id', async (request,response) => {
+    const resultado = await db.query('SELECT * FROM usuarios WHERE id = $1', [request.params.id]);
+    response.status(200).json(resultado.rows[0])
 });
 
-router.post('/', (request,response) => {
-    usuarios.push(request.body)
+router.post('/', async (request,response) => {
+    const { nome, cpf, telefone } = request.body;
+    await db.query('INSERT INTO usuarios (nome, cpf, telefone) VALUES ($1, $2, $3)', [nome, cpf, telefone]);
     response.status(201).send('Usuário Cadastrado')
+});
+
+router.put('/:id', async (request, response) => {
+    const { nome, cpf, telefone } = request.body;
+    await db.query('UPDATE usuarios SET nome=$1, cpf=$2, telefone=$3 WHERE id=$4', [nome, cpf, telefone, request.params.id]);
+    response.status(200).send('Usuário Atualizado');
+});
+
+router.delete('/:id', async (request, response) => {
+    await db.query('DELETE FROM usuarios WHERE id=$1', [request.params.id]);
+    response.status(200).send('Usuário Deletado');
 });
 
 
